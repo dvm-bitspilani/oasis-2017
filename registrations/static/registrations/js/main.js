1fdc73283d9a20befb0c8a103eec6ab64dbf2dba
Roboneet <neethumariyajoy@gmail.com>
2017-09-23 04:49:39 +0530

    glitch fixes

--- registrations/static/registrations/js/main.js
+++ registrations/static/registrations/js/main.js
@@ -15,6 +15,7 @@ var halfHeight = $(window).height()/2;
 function changeActiveField(){
   $('.active').removeClass('active');
   var activeField = $('.field:eq(' + index + ')')
+  console.log(activeField, "change")
   activeField.addClass('active'); 
   activeField.find('input').focus();
   // console.log("changeActiveField")
@@ -23,21 +24,24 @@ function changeActiveField(){
 
 // To scroll to a particular field 
 function scrollToActiveField(field){  
-  console.log($(field),$(field).index())
-  var newIndex = $(field).index();
-  // console.log(newIndex)
+  console.log($(field),$(field).index() )
+  var newIndex = $(field).index() - 1;
+  console.log(newIndex)
   if(newIndex != index){
     index = newIndex;
     var offset = $(field).offset().top;
     var screen  = $('html,body');
-    screen.animate({scrollTop: screen.scrollTop() + offset - halfHeight + topPadding}, 300)
+    // screen.animate({scrollTop: screen.scrollTop() + offset - halfHeight + topPadding}, 300)
+    screen.animate({scrollTop: $(field).offset().top + $(field).height()/2 - $(window).height()/2}, 300)
     // console.log("scrollToActiveField")
     scrollCheck = 0;
-    changeActiveField();
+    // changeActiveField();
   }
 }
 
 $(document).ready(function(){
+console.log($('#select_info'));
+$('#select_info').text('(Press Ctrl+ ( command on mac ) to add more than one event)');
 // $.ajax({
 //   method:"GET",
 //   url:""
@@ -87,11 +91,12 @@ wrapper = $('.wrapper');
           }
         }
         else{
-        if((scroll >= $(fields[i]).offset().top - halfHeight + topPadding) && (scroll <= $(fields[i+1]).offset().top - halfHeight+topPadding))
-        { 
-            index = i;
-            changeActiveField();
-        }
+          // if((scroll >= $(fields[i]).offset().top - halfHeight + topPadding) && (scroll <= $(fields[i+1]).offset().top - halfHeight+topPadding))
+          if((scroll + halfHeight > $(fields[i]).offset().top ) && (scroll + halfHeight < $(fields[i]).offset().top + $(fields[i]).height()))
+          { 
+              index = i;
+              changeActiveField();
+          }
         }
       }
     }
@@ -100,10 +105,25 @@ wrapper = $('.wrapper');
   // To move to a particular field on focusing on an input
     var inputs = $('.field input');
 
-    // inputs.focus(function(){
-    //   console.log("focuscallback")
-    //   scrollToActiveField($(this).parent().parent());
-    // });
+    inputs.focus(function(){
+      console.log("focuscallback")
+      console.log("inputs",this)
+      scrollToActiveField($(this).parent().parent());
+    });
+
+    var target_labels = $('.field input[type=radio] + label');
+    target_labels.focus(function(){
+      console.log("focuscallback")
+      console.log("labels ...",$(this).parent().parent())
+      scrollToActiveField($(this).parent().parent());
+    });
+
+    var select = $('.field select');
+    select.focus(function(){
+      console.log("focuscallback")
+      console.log("select ...",$(this).parent().parent())
+      scrollToActiveField($(this).parent().parent());
+    })
 
     // To move to next field on keydown or Enter
     var temp;
