bd5308cb12295125c89b098277ee25f429410ff9
Megh-Thakkar <megh.1211@gmail.com>
2017-10-04 14:03:41 +0530

    Event jsonresponse info

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 				# url(r'^$', views.create_user, name="cr_approval"),
-				url(r'^events$', views.all_events, name="all_events"),
-				url(r'^event/(?P<e_id>\d+)$', views.get_event, name="get_event"),
+				url(r'^events/$', views.all_events, name="all_events"),
+				url(r'^events/(?P<e_id>\d+)/$', views.get_event, name="get_event"),
 
 				]
