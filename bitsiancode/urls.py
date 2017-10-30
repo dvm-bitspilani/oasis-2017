0001111c761c32118a5a8b11420f54f5592f42d7
Megh-Thakkar <megh.1211@gmail.com>
2017-10-30 19:37:34 +0530

    Bitsian code oauth

---
+++ bitsiancode/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from bitsiancode import views
+app_name = 'bitsiancode'
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+    url(r'^get_barcode/$', views.get_barcode, name='barcode'),
+]
--- oasis2017/settings.py
+++ oasis2017/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'wordwars',
     'api',
     'store',
+    'bitsiancode',
 ]
 
 MIDDLEWARE = [
