71e851345deed6670ffcca47d2b570170d665d3e
Megh-Thakkar <megh.1211@gmail.com>
2017-10-31 03:38:34 +0530

    change

--- registrations/models.py
+++ registrations/models.py
@@ -101,7 +101,7 @@ class Group(models.Model):
 
 	amount_deduct = models.IntegerField(default=0)
 	created_time = models.DateTimeField(auto_now=True)
-	group_code = models.CharField(max_length=20, null=True, blank=True)
+	group_code = models.CharField(max_length=50, null=True, blank=True)
 
 class CheckoutGroup(models.Model):
 
