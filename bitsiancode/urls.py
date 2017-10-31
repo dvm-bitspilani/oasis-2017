ca909d59bce80e443b4d24a44f98d381e4f54001
Megh-Thakkar <megh.1211@gmail.com>
2017-11-01 02:37:14 +0530

    Bitsiancode

--- bitsiancode/urls.py
+++ bitsiancode/urls.py
@@ -1,8 +1,8 @@
-# from django.conf.urls import url
-# from bitsiancode import views
-# app_name = 'bitsiancode'
+from django.conf.urls import url
+from bitsiancode import views
+app_name = 'bitsiancode'
 
-# urlpatterns = [
-# 	url(r'^$', views.index, name='index'),
-#     url(r'^get_barcode/$', views.get_barcode, name='barcode'),
-# ]
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+    url(r'^get_barcode/$', views.get_barcode, name='barcode'),
+]
--- oasis2017/settings.py
+++ oasis2017/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'wordwars',
     'api',
     'store',
-    # 'bitsiancode',
+    'bitsiancode',
 ]
 
 MIDDLEWARE = [
