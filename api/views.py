10a56e8e559d8c04304be284018f9e41b2f40a61
Megh-Thakkar <megh.1211@gmail.com>
2017-11-01 12:15:37 +0530

    change

--- api/views.py
+++ api/views.py
@@ -308,6 +308,10 @@ def get_profile(request):
 	participant = Participant.objects.get(user=request.user)
 	if not participant.firewallz_passed:
 		return Response({'message':'Register at Firewallz first.'})
+	if participant.is_guest: 
+		participant_serializer = ProfileSerializer(participant, context={'request':request})
+		profshow_serializer = AttendanceSerializer(Attendance.objects.filter(participant=participant), many=True)
+		return Response({'participant':participant_serializer.data, 'prof_shows':profshow_serializer.data})
 	event_set = [participation.event for participation in Participation.objects.filter(participant=participant, pcr_approved=True)]
 	event_serializer = EventSerializer(event_set, many=True)
 	participant_serializer = ProfileSerializer(participant, context={'request':request})
